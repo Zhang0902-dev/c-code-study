130336e3cee4dc3e5ff331180c8178fd4af83fe0
Zhang0902-dev <3928495739@qq.com>
2026-08-30 20:47:43 +0800

    Create text.c
    新增hello.c，第一个C语言程序

---
+++ text.c
@@ -0,0 +1,14 @@
+#include <stdio.h>//std-标准standard input output
+
+
+//int 是整型的意思，main前面的int表示main函数调用之后返回一个整型值
+int main()//main主函数-程序的入口,有且只有一个
+{
+	printf("hello world\n");//在这里完成任务，printf即输出hello world，print打印function函数
+	//printf是库函数，需要调取数据库，数据库为#include <stdio.h>
+
+
+
+	//     \n是输出完换行
+		return 0;//返回 0
+}
